650c26fde2596109e7229a0d6c7fcd80875f6a3e
AdrianMartiR <adrianmarti06@gmail.com>
2018-03-24 18:21:35 +0100

    fixed bug by replacing {} with []

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 from setuptools import find_packages
 
-install_requires = {
+install_requires = [
     'numpy',
     'scipy',
     'tqdm'
-}
+]
 
 setup(
     name="capslayer",
